86535abf67adbb2f93985e5ec8f7876c4094f03b
Omniamutanturnihilinterit <92959023+Omniamutanturnihilinterit@users.noreply.github.com>
2022-02-04 17:50:28 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Kalkulator Sederhana Dengan Vuejs</h1>
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator";
+
+export default {
+  name: "App",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
